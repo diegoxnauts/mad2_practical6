1d16e194394ea3bab3ca7c6b3d236b13ef2c9d05
John Michael San Diego <jmsandiego0710@gmail.com>
2019-11-26 20:57:32 +0800

    Initial Commit

---
+++ mad2_practical6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mad2_practical6
+//
+//  Created by Jm San Diego on 26/11/19.
+//  Copyright © 2019 Jm San Diego. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
